9f614a0dde5b468b426c1e5a14d0b143cf895d98
Ruta98 <anilarut@gmail.com>
2020-01-05 10:05:30 +0100

    add InputOutputData.cs

---
+++ Bowling/Classes/InputOutputData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bowling.Classes
+{
+    class InputOutputData : BaseViewModel
+    {
+        string inputText;
+        public string InputText
+        {
+            get => inputText;
+            set
+            {
+                inputText = value;
+                OnPropertyChanged(nameof(InputText));
+            }
+        }
+        string outputText;
+        public string OutputText
+        {
+            get => outputText;
+            set
+            {
+                outputText = value;
+                OnPropertyChanged(nameof(OutputText));
+            }
+        }
+    }
+}
